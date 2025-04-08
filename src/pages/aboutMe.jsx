343d3702d591edb1cafa044e52597d6ef64af7b5
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-04-08 17:28:20 -0400

    added pics

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -4,7 +4,11 @@ const AboutMe = () => {
       <h1>About Me</h1>
       <p>Hi, I'm Orlando. I used to be a HVAC Service Technician.</p>
       <p>I have a passion for hvac, learning new video games, diving deeper into my new career as a Full stack developer.</p>
-      <p>Here I'll go over the tech stack I have utilized in this project.</p>
+      <p>This project is me sprouting out after a successful coding bootcamp at FullStack Academy. For the project I wanted
+         to touch all the basics, and learn new (to me) tech on the fly! I originally started out with JavaScript, but eventually
+         decided on something a little more risky... TypeScript, while learning it on the fly. This also ushered in other tech stack 
+         changes, such as NextJS. I'm storing my data in PostgreSQL, using Vite for quick development, along with Next.js as the 
+         framework. </p>
     </div>
   );
 }
--- src/pages/hvacInfo.jsx
+++ src/pages/hvacInfo.jsx
@@ -2,7 +2,7 @@ const HvacInfo = () => {
   return (
     <div>
       <h1>HVAC Information</h1>
-      <p>This is the HVAC information page.</p>
+      <p>For the most part, people don't know they have hvac issues until it's too late!</p>
       <p>Here you can find details about heating, ventilation, and air conditioning systems.</p>
       <p>Going over tools and products I have, or have used, and their purpose to meet your HVAC needs!</p>
     </div>
