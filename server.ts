647c714e8862eece67730a3081ff98c2d8d16e26
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-04-13 15:37:14 -0400

    adjusted server file to incorporate needed funcs for SSR

--- server.ts
+++ server.ts
@@ -1,16 +1,24 @@
 import 'dotenv/config';
 import express from 'express';
-const app = express();
 import cors from 'cors';
-import { router as toolsRoutes} from './routes/tools.js';
-import { router as usersRoutes} from './routes/users.js';
-import { router as pagesRoutes} from './routes/pages.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { router as toolsRoutes } from './routes/tools.js';
+import { router as usersRoutes } from './routes/users.js';
+import { router as pagesRoutes } from './routes/pages.js';
 import triviaRoutes from './routes/trivia.js';
 
-app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(cors()); 
- 
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(express.json());
+app.use(cors());
+
 app.use('/', pagesRoutes);
 app.use('/api/tools', toolsRoutes);
 app.use('/api/users', usersRoutes);
@@ -18,5 +26,9 @@ app.use('/api', triviaRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 export default app;
