7b569dab31abde41be687c47a806c3bca1a1e153
Orlando Pacheco Jr <orlando.pacheco906@gmail.com>
2025-04-14 17:56:44 -0400

    testing ping/pong

--- server.ts
+++ server.ts
@@ -1,39 +1,14 @@
-import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// import toolsRoutes from './routes/tools.js';
-import usersRoutes from './routes/users.js';
-// import pagesRoutes from './routes/pages.js';
-import triviaRoutes from './routes/trivia.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  console.log(`[${req.method}] ${req.path}`);
-  next();
+app.get('/ping', (req, res) => {
+  res.send('pong');
 });
-
-// app.use('/api/pages', pagesRoutes);
-// app.use('/api/tools', toolsRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/trivia', triviaRoutes);
-
-app.use(express.static(path.join(__dirname, 'dist', 'client')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'client', 'index.html'));
-// });
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-export default app;
